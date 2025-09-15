77f4f691f354f3b2d9dff440235ea6c1a6d0c99b
Sneha Singh <pc.snehasingh@gmail.com>
2025-09-15 05:41:28 +0530

    Changed path of csv file

--- QRcognizant.py
+++ QRcognizant.py
@@ -4,13 +4,12 @@ import os
 
 app = Flask(__name__)
 
-# Load registered PRNs
-registered_df = pd.read_csv(r'C:\Users\Sneha Singh\OneDrive\Desktop\Projects\QRcognizant2026RG\RGCognizant-UG2026.csv')
+registered_df = pd.read_csv('RGCognizant-UG2026.csv')
 registered_df.columns = registered_df.columns.str.strip()
 registered_prns = set(registered_df['PRN'].astype(str))
 
-# Load used PRNs (or create empty set if file doesn't exist)
-used_prns_file = r'C:\Users\Sneha Singh\OneDrive\Desktop\Projects\QRcognizant2026RG\used_prns.csv'
+used_prns_file = 'used_prns.csv'
+
 if os.path.exists(used_prns_file):
     used_df = pd.read_csv(used_prns_file)
     used_prns = set(used_df['PRN'].astype(str))
